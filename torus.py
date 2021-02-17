ef361704e2e0f66a5e36634fc8a8298f3bc70fe6
IstominArseniy <istomin15arseniy@gmail.com>
2021-02-17 18:26:02 +0300

    one third of a torus

--- torus.py
+++ torus.py
@@ -1,8 +1,25 @@
 import gmsh
+import math
 
 gmsh.initialize()
 gmsh.model.add("torus")
-lc = 1e-1
+lc = 0.5
+center = gmsh.model.geo.addPoint(10, 0, 0)
+p1 = gmsh.model.geo.addPoint(10, 0, 1, lc)
+p2 = gmsh.model.geo.addPoint(10 - 3**0.5 / 2, 0, -0.5, lc)
+p3 = gmsh.model.geo.addPoint(10 + 3**0.5 / 2, 0, -0.5, lc)
+# basic circle
+arc1 = gmsh.model.geo.addCircleArc(p1, center, p2)
+arc2 = gmsh.model.geo.addCircleArc(p2, center, p3)
+arc3 = gmsh.model.geo.addCircleArc(p3, center, p1)
+circle = gmsh.model.geo.addCurveLoop([arc1, arc2, arc3])
+print(circle)
+# circle_plane = gmsh.model.geo.addPlaneSurface([circle])
+# external torus
+torus1_1 = gmsh.model.geo.revolve([(1, arc1), (1, arc2), (1, arc3)], 0, 0, 0, 0, 0, 1, 2/3 * math.pi)
+print(torus1_1)
+# torus1_2 = gmsh.model.geo.revolve(torus1_1, 0, 0, 0, 0, 0, 1, 2/3 * math.pi)
+# torus1_3 = gmsh.model.geo.revolve([(1, arc1), (1, arc2), (1, arc3)], 0, 0, 0, 0, 0, 1, 2/3 * math.pi)
 
 # view
 gmsh.model.geo.synchronize()
